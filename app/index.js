7459976b190f05f9ff65610b48556db0bb06e7d6
Fiat Bruno <fiatbruno10@gmail.com>
2023-05-09 10:57:33 +0200

    Implementing navigation component

--- app/index.js
+++ app/index.js
@@ -10,17 +10,48 @@ import {
     Welcome,
 } from "../components"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { ScrollView } from "react-native-gesture-handler"
 
 const Home = () => {
     const router = useRouter()
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+        <SafeAreaView
+            style={{ flex: 1, backgroundColor: COLORS.lightWhite }}
+        >
             <Stack.Screen
                 options={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerStyle: {
+                        backgroundColor: COLORS.lightWhite,
+                    },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.menu}
+                            dimension="60%"
+                        />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={images.profile}
+                            dimension="100%"
+                        />
+                    ),
+                    headerTitle: "",
                 }}
             />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium,
+                    }}
+                >
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
