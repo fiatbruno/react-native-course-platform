f47877a21aa2c187891b742471b4eab6964d61b5
Fiat Bruno <fiatbruno10@gmail.com>
2023-05-15 11:23:07 +0200

    Utility fn to replace unknown logos

--- components/common/cards/popular/PopularCourseCard.jsx
+++ components/common/cards/popular/PopularCourseCard.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { View, Text, TouchableOpacity, Image } from "react-native"
+import { checkImageURL } from "../../../../utils"
 
 import styles from "./popularcoursecard.style"
 
@@ -17,7 +18,11 @@ const PopularCourseCard = ({
                 style={styles.logoContainer(selectedCourse, item)}
             >
                 <Image
-                    source={{ uri: item.employer_logo }}
+                    source={{
+                        uri: checkImageURL(item.employer_logo)
+                            ? item.employer_logo
+                            : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+                    }}
                     resizeMode="contain"
                     style={styles.logoImage}
                 ></Image>
@@ -26,8 +31,16 @@ const PopularCourseCard = ({
                 {item.employer_name}
             </Text>
             <View style={styles.infoContainer}>
-                <Text style={styles.jobName(selectedCourse, item)} numberOfLines={1}>{item.job_title}</Text>
-                <Text style={styles.location}> {item.job_country}</Text>
+                <Text
+                    style={styles.jobName(selectedCourse, item)}
+                    numberOfLines={1}
+                >
+                    {item.job_title}
+                </Text>
+                <Text style={styles.location}>
+                    {" "}
+                    {item.job_country}
+                </Text>
             </View>
         </TouchableOpacity>
     )
